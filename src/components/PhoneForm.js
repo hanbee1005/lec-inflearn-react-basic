d12118e67d195a92fafeaf366ed094ed508bb494
hanbee <sonhanbi1002@gmail.com>
2021-06-22 01:18:47 +0900

    ref 를 사용하는 또 다른 방식

--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class PhoneForm extends Component {
-    input = null;
+    input = React.createRef();
 
     state = {
         name: '',
@@ -22,7 +22,7 @@ class PhoneForm extends Component {
             name: '',
             phone: ''
         });
-        this.input.focus();
+        this.input.current.focus();
     }
 
     render() {
@@ -33,7 +33,7 @@ class PhoneForm extends Component {
                     placeholder="이름" 
                     onChange={this.handleChange} 
                     value={this.state.name}
-                    ref={ref => this.input = ref}
+                    ref={this.input}
                 />
                 <input 
                     name="phone"
